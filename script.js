68d8c8315576b581e233ce557f804b4d3f53582a
Mohammed Saif <mohdsaifwork19@gmail.com>
2026-02-12 19:17:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const timer = document.getElementById("timer");
+
+function updateTimer(){
+	const date = new Date();
+	timer.textContent = date.toLocaleString();
+}
+
+updateTimer();
+
+setInterval(updateTimer, 1000);
